ca1888eec16eac16be7e740fbac9a3b718c279fd
Joaoenaeli05 <naelisdasilva@gmail.com>
2023-12-28 02:17:29 -0300

    atualizando

--- src/chess_system/Application.java
+++ src/chess_system/Application.java
@@ -1,13 +1,29 @@
 package chess_system;
 
-import chess_system.boardGame.Board;
 import chess_system.chess.ChessMatch;
-import chess_system.UI;
+import chess_system.chess.ChessPiece;
+import chess_system.chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Application {
  public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
 
   ChessMatch chessMatch = new ChessMatch();
-  UI.printBoard(chessMatch.getPieces());
+  while (true) {
+   UI.printBoard(chessMatch.getPieces());
+   System.out.println();
+   System.out.print("Source: ");
+   ChessPosition source = UI.readChessPosition(sc);
+
+   System.out.println();
+   System.out.print("Target: ");
+   ChessPosition target = UI.readChessPosition(sc);
+
+   ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+
+  }
  }
 }
