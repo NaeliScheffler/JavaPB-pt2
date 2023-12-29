886fe95985f3d6a4edeb945db88956dc4aaf5b1e
Joaoenaeli05 <naelisdasilva@gmail.com>
2023-12-28 22:38:17 -0300

    atualizando

--- src/squad_att/Atividade_30.java
+++ src/squad_att/Atividade_30.java
@@ -2,6 +2,9 @@ package squad_att;
 
 import java.util.Scanner;
 
+//Crie um programa que verifique se uma string é um palíndromo
+//ignorando espaços em branco e maiúsculas/minúsculas.
+
 public class Atividade_30 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -9,4 +12,18 @@ public class Atividade_30 {
         System.out.println("Insira uma frase para verificar se é um palíndromo ou não: ");
         String frase = scanner.nextLine();
 
+        if (palindromo(frase))
+            System.out.println("A frase fornecida é um palíndromo ");
+        else System.out.println("A frase fornecida não é um palíndromo ");
+
+    }
+
+    public static boolean palindromo(String frase) {
+        // Remove espaços em branco e converte a frase para minúsculas
+        String semEspacosMinuscula = frase.replaceAll("\\s", "").toLowerCase();
+
+        // Verifica se a string invertida é igual à original
+        String invertida = new StringBuilder(semEspacosMinuscula).reverse().toString();
+        return semEspacosMinuscula.equals(invertida);
+    }
 }
